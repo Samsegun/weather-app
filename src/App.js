73d3fdd33acf5afb43fcadec931689d6b8f2df9d
Samsegun <oyebadesamsegun@gmail.com>
2022-01-24 19:07:56 +0100

    api call added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+import { API_KEY } from "./API";
+import styles from "./App.module.css";
+
 const App = () => {
-  return <div className="App">hello</div>;
+  // console.log(API_KEY);
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/find?q=London,uk&units=metric&appid=${API_KEY}`
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw Error("could not fetch data");
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => {
+        // destructure data from api
+        // const {name} = data.list;
+        console.log(data);
+        console.log(data.list);
+        console.log(data.list[0].name);
+        console.log(data.list[0].main);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return <div className={styles.wrapper}>hello</div>;
 };
 
 export default App;
