b6219b11910d13a42244e2f2df0eba058f46f65f
Samsegun <oyebadesamsegun@gmail.com>
2022-01-25 22:40:27 +0100

    fetch requets added

--- src/App.js
+++ src/App.js
@@ -3,45 +3,23 @@ import { API_KEY } from "./API";
 import styles from "./App.module.css";
 
 const App = () => {
-  // console.log(API_KEY);
-
   useEffect(() => {
-    // fetch(
-    //   `https://api.openweathermap.org/data/2.5/find?q=accra,ghana&units=metric&appid=${API_KEY}`
-    // )
-    //   .then(response => {
-    //     if (!response.ok) {
-    //       throw Error("could not fetch data");
-    //     } else {
-    //       return response.json();
-    //     }
-    //   })
-    //   .then(data => {
-    //     // destructure data from api
-    //     console.log(data);
-    //     const { all: clouds } = data.list[0].clouds;
-    //     const { temp, humidity } = data.list[0].main;
-    //     const name = data.list[0].name;
-    //     const { country } = data.list[0].sys;
-    //     const { description: weatherDesc } = data.list[0].weather[0];
-    //     console.log({ temp, humidity, name, country, clouds, weatherDesc });
-    //   })
-    //   .catch(err => console.log(err));
-    // fetch("http://ip-api.com/json")
-    //   .then(response => response.json())
-    //   .then(data => console.group(data));
     Promise.all([
       fetch(
         `https://api.openweathermap.org/data/2.5/find?q=accra,ghana&units=metric&appid=${API_KEY}`
-      ),
-      fetch("http://ip-api.com/json"),
+      ).then(response => response.json()),
+      fetch("http://ip-api.com/json").then(response => response.json()),
     ])
-      .then(([weather, location]) => {
-        console.log(weather, location);
+      .then(value => {
+        const [weather, location] = value;
+
+        return { weather, location };
       })
-      .catch(err => {
-        console.log(err);
-      });
+      .then(data => {
+        const { location, weather } = data;
+        console.log(location, weather);
+      })
+      .catch(err => console.log(err));
   }, []);
 
   return <div className={styles.wrapper}>hello</div>;
