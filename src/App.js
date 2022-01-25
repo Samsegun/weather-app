78c513e270998c4fbd82ccebb4ecd027291003d1
Samsegun <oyebadesamsegun@gmail.com>
2022-01-25 18:44:50 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -6,25 +6,42 @@ const App = () => {
   // console.log(API_KEY);
 
   useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/find?q=London,uk&units=metric&appid=${API_KEY}`
-    )
-      .then(response => {
-        if (!response.ok) {
-          throw Error("could not fetch data");
-        } else {
-          return response.json();
-        }
+    // fetch(
+    //   `https://api.openweathermap.org/data/2.5/find?q=accra,ghana&units=metric&appid=${API_KEY}`
+    // )
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw Error("could not fetch data");
+    //     } else {
+    //       return response.json();
+    //     }
+    //   })
+    //   .then(data => {
+    //     // destructure data from api
+    //     console.log(data);
+    //     const { all: clouds } = data.list[0].clouds;
+    //     const { temp, humidity } = data.list[0].main;
+    //     const name = data.list[0].name;
+    //     const { country } = data.list[0].sys;
+    //     const { description: weatherDesc } = data.list[0].weather[0];
+    //     console.log({ temp, humidity, name, country, clouds, weatherDesc });
+    //   })
+    //   .catch(err => console.log(err));
+    // fetch("http://ip-api.com/json")
+    //   .then(response => response.json())
+    //   .then(data => console.group(data));
+    Promise.all([
+      fetch(
+        `https://api.openweathermap.org/data/2.5/find?q=accra,ghana&units=metric&appid=${API_KEY}`
+      ),
+      fetch("http://ip-api.com/json"),
+    ])
+      .then(([weather, location]) => {
+        console.log(weather, location);
       })
-      .then(data => {
-        // destructure data from api
-        // const {name} = data.list;
-        console.log(data);
-        console.log(data.list);
-        console.log(data.list[0].name);
-        console.log(data.list[0].main);
-      })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
 
   return <div className={styles.wrapper}>hello</div>;
